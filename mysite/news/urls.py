a27ecb3035a00e85c8a30ca91f97c7d6e451167b
Andrew <klim4uk.andrew@gmail.com>
2019-05-30 22:30:06 +0300

    change urls, views and html

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -4,8 +4,7 @@ from news.models import Articles
 
 urlpatterns = [
        url(r'^$',
-           ListView.as_view(queryset = Articles.objects.all().order_by("-date")[:20],
-            template_name = "news/posts.html")),
-
+           ListView.as_view(queryset = Articles.objects.all().order_by('-date')[:20],
+            template_name = 'news/posts.html')),
 
 ]
