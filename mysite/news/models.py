d817830ec14ac373a22ca78c1c15a7be3993a33c
Andrew Klimchuk <46954361+Klim4uk@users.noreply.github.com>
2019-06-07 18:08:40 +0300

    change html

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -5,6 +5,7 @@ class Articles(models.Model):
     title = models.CharField(max_length= 120)
     post = models.TextField()
     data = models.DateTimeField()
+    foto = models.TextField()
 
     def __str__(self):
         return self.title
