5ae304b964e1d3ee649a2dcea57a6b65ebb5a73a
Andrew Klimchuk <46954361+Klim4uk@users.noreply.github.com>
2019-08-13 22:38:11 +0300

    Update Admin panel

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Articles
 
-admin.site.register(Articles)
+
+class ArticlesAdmin(admin.ModelAdmin):
+
+    list_display = ['author', 'title', 'data']
+    list_filter = ['data', 'title', 'author']
+    search_fields = ['title']
+
+
+    class Meta:
+        model = Articles
+
+
+admin.site.register(Articles, ArticlesAdmin)
 
 
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 
 class Articles(models.Model):
-    title = models.CharField(max_length= 120)
-    post = models.TextField()
-    data = models.DateTimeField()
+    title = models.CharField(max_length= 120, verbose_name='Заголовок')
+    post = models.TextField(verbose_name= 'Текст')
+    author = models.CharField(max_length= 30, verbose_name='Автор')
+    data = models.DateTimeField(verbose_name='Дата')
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
 
